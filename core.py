0678c3da3fc1377f703fdfabcebe3acb1237749c
TheLegendary1248 <1248alex@gmail.com>
2023-05-29 13:11:20 -0400

    Save folder structure plan as html

--- core.py
+++ core.py
@@ -7,13 +7,19 @@ app.setStyle('Fusion')
 app.setApplicationName('Online Timeline')
 win=QWidget()
 win.setBaseSize(200,200)
-def SetNewTextView()
+def SetNewTextView(label: QLabel, text: str) -> None:
+    label.text
 def StartUp():
 
     layout=QVBoxLayout()
+    
+    fileDialog = QFileDialog(caption="Hello?")
+    fileDialog.setAcceptMode(QFileDialog.AcceptMode.AcceptOpen)
+    print(f"$name?: {fileDialog.selectedFiles()}");
+    layout.addWidget(fileDialog)
     label=QLabel('Please wait a moment while the program loads', alignment=Qt.AlignmentFlag.AlignCenter)
     layout.addWidget(label)
-
+    fileDialog.open
     win.setLayout(layout)
     win.show()
 
