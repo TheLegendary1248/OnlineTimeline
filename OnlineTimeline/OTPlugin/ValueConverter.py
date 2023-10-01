c1ff13cb18442559e13c1e751528d74c467dbb79
TheLegendary1248 <1248alex@gmail.com>
2023-10-01 10:58:16 -0400

    Convert time in ValueConverter. Likely, very not worky

--- OnlineTimeline/OTPlugin/ValueConverter.py
+++ OnlineTimeline/OTPlugin/ValueConverter.py
@@ -5,48 +5,71 @@ import ast
 import json
 from pprint import pprint
 
+# ---BASE CONVERTER DEFINITION---
 class ValueConverter():
     """This class is for value conversions, usually for parsing strings"""
-    registered:dict = {}
+    registered:dict[str, ValueConverter.__class__] = {}
     hasCachedConverters = False
     name = "base"
     def __init__(self) -> None:
         pass
     
-    def LoadConfig(self) -> None:
+    def LoadConfig(self, config: dict) -> None:
+        self.config = config
         pass
 
     def ConvertValue(self, val) -> None:
         raise NotImplementedError("Base cannot be used for conversion")
 
     @staticmethod
-    def RegisterConverters(name: str, c: ValueConverter):
+    def RegisterConverters():
         """Registers converters"""
-        ValueConverter.registered[name] = c
+        ValueConverter.hasCachedConverters = True
+        all_subclasses = ValueConverter.__subclasses__()
+        for subClass in all_subclasses:
+            ValueConverter.registered[subClass.name] = subClass
         pass
 
     @staticmethod
-    def ConvertValue(name: str, val, config: dict):
+    def ConvertValue(type: str, value, config: dict):
         """Convert the value with said config"""
-        all_subclasses = ValueConverter.__subclasses__()
-        converter = ValueConverter.registered[str]
-        pass
+        if not ValueConverter.hasCachedConverters:
+            ValueConverter.RegisterConverters()
 
+        converter = ValueConverter.registered[type]()
+        converter.LoadConfig(config)
+        pprint(f"handed down config is {config}")
+        return converter.ConvertValue(value)
+
+# ---BUILTIN CONVERTERS---
 class TimeConverter(ValueConverter):
+    import datetime
+    name = "time"
+    methods:dict[str, function] = {
+        "strptime": datetime.datetime.strptime,
+        "fromiso": datetime.datetime.fromisoformat, 
+        "parse" : lambda: None
+    }
     def ConvertValue(self, val) -> None:
-        
-        return
-    def LoadConfig(self, val: dict) -> None:
-        return super().LoadConfig()
-        # return super().ConvertValue(val)
+        func = self.methods[self.config["method"]]
+        #TEMP: Tried https://stackoverflow.com/questions/24463202/typeerror-get-takes-no-keyword-arguments. Will fix later
+        # return func(val, **self.config)
+        return func(val, self.config["format"]).timestamp()
+    def LoadConfig(self, config: dict) -> None:
+        return super().LoadConfig(config)
     pass
 
 class EnumConverter(ValueConverter):
+    name = "enum"
     def ConvertValue(self, val) -> None:
         values: dict = None
         return values["val"]
-        # return super().ConvertValue(val)
+    def LoadConfig(self, val: dict) -> None:
+        return super().LoadConfig()
     pass
+
+class NumberConverter(ValueConverter):
+    name = ""
 
 class ValueConverterArgs(argparse.Namespace):
     value: any
@@ -66,10 +89,9 @@ if __name__ == '__main__':
     namespace = ValueConverterArgs()
     #If send to timeline
     arguments = parser.parse_args(namespace=namespace)
-
-    
+    argumentsVars = vars(arguments)
     # Count how many arguments have a non-None value
     argCount = [v != None for v in vars(arguments).values()].count(True)
     onlyOneArg = argCount == 1
     
-    pprint(arguments)
+    pprint(ValueConverter.ConvertValue(**argumentsVars))
